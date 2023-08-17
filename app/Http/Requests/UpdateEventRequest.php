3d9064915fdd9cac8feefdef182b6eead64be07a
chalaa <chalaolanigeleta@gmail.com>
2023-08-17 13:25:05 +0300

    Add Update event request

---
+++ app/Http/Requests/UpdateEventRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateEventRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "title" => ["string"],
+            "user_id"=>[],
+            "descripton" => ["string"],
+            "start_date" => ["date","after:tomorrow"],
+            "end_date" => ["date","after:tomorrow"],
+            "registration_start_date" => ["date","before:yesterday"],
+            "registration_end_date" => ["date","before:yesterday"],
+            "location" => ["string"],
+            "hall" => ["string"],
+            "event_type" => ["boolean"],
+            "event_status" => ["string"],
+            "available_seat" => ["integer"]
+        ];
+    }
+}
