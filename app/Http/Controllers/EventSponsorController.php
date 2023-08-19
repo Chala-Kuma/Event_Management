094ffee88911daf5cbb991046fa5616d4372ae4c
chalaa <chalaolanigeleta@gmail.com>
2023-08-19 16:13:45 +0300

    Add EventSponsor Apiresource and EventSponsorController functions

--- app/Http/Controllers/EventSponsorController.php
+++ app/Http/Controllers/EventSponsorController.php
@@ -2,32 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event;
+use App\Models\Sponsor;
+use App\Models\EventSponsor;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class EventSponsorController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Event $event)
     {
         //
+        return $event->sponsor->all();
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request, Event $event)
     {
         //
+        $request->validate([
+            "sponsor_id" => ["required","integer"]
+        ]);
+
+        return EventSponsor::create([
+            "event_id" => $event->id,
+            "sponsor_id" => $request->sponsor_id
+        ]);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Event $event, Sponsor $sponsor)
     {
         //
+        foreach ($event->speaker as $onesponsor){
+            if ($onesponsor->id == $sponsor->id){
+                return $onesponsor;
+            }
+        }
+        return response(["message"=>"sponsor not found"],Response::HTTP_NOT_FOUND);
     }
 
     /**
@@ -41,8 +60,10 @@ class EventSponsorController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Event $event, Sponsor $sponsor)
     {
         //
+        EventSponsor::where("event_id",$event->id)->where("speaker_id", $sponsor->id)->delete();
+        return response([],Response::HTTP_NO_CONTENT);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\EventSpeakerController;
+use App\Http\Controllers\EventSponsorController;
 use App\Http\Controllers\SpeakerController;
 use App\Http\Controllers\SponsorController;
 
@@ -23,9 +24,12 @@ Route::middleware("auth:sanctum")->group(function(){
     Route::apiResource("speaker",SpeakerController::class,["except"=>["show"]]);
     Route::apiResource("sponsor",SponsorController::class,["except"=>["show"]]);
     Route::apiResource("event/{event}/speaker",EventSpeakerController::class,["except"=>["index","show"]]);
+    Route::apiResource("event/{event}/sponsor",EventSponsorController::class,["except"=>["index","show"]]);
+
 });
 Route::apiResource("event",EventController::class,["only"=>["index","show"]]);
 Route::apiResource("event/{event}/speaker",EventSpeakerController::class,["only"=>["index","show"]]);
+Route::apiResource("event/{event}/sponsor",EventSponsorController::class,["only"=>["index","show"]]);
 Route::apiResource("speaker",SpeakerController::class,["only"=>["show"]]);
 Route::apiResource("sponsor",SponsorController::class,["only"=>["show"]]);
 Route::post("auth/register",[AuthController::class, "register"]);
