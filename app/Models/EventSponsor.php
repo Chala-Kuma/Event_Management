ac09e151acba623c022ef5614a9de424a61fd685
chalaa <chalaolanigeleta@gmail.com>
2023-08-19 13:33:24 +0300

    Add Softdelete trait to the Models

--- app/Models/EventSponsor.php
+++ app/Models/EventSponsor.php
@@ -4,15 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class EventSponsor extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
 
     protected $fillable = [
         "event_id",
         "sponsor_id"
     ];
 
-    
+
 }
