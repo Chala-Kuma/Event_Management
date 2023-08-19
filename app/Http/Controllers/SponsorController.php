1e86451fb109488bdfca9344967e3eda81571432
chalaa <chalaolanigeleta@gmail.com>
2023-08-19 13:35:21 +0300

    Add Sponsor Apiresouce and all functions in the controller

--- app/Http/Controllers/SponsorController.php
+++ app/Http/Controllers/SponsorController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Sponsor;
 use Illuminate\Http\Request;
+use App\Http\Requests\SponsorRequest;
+use Illuminate\Support\Facades\Storage;
+use App\Http\Requests\UpdateSponsorRequest;
+use Symfony\Component\HttpFoundation\Response;
 
 class SponsorController extends Controller
 {
@@ -12,37 +17,63 @@ class SponsorController extends Controller
     public function index()
     {
         //
+        return Sponsor::all();
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(SponsorRequest $request)
     {
         //
+        $request->validated();
+        return auth()->user()->sponsor()->create([
+            "name"=>$request->name,
+            "email"=>$request->email,
+            "logo"=> $request->file('logo')->store("sponsor logo","public")
+        ]);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Sponsor $sponsor)
     {
         //
+        return $sponsor;
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateSponsorRequest $request, Sponsor $sponsor)
     {
         //
+        $request->validated();
+        if($request->hasFile('logo')){
+            Storage::disk("public")->delete($sponsor->logo);
+            $sponsor->update([
+                "name"=>$request->name?$request->name:$sponsor->name,
+                "email" =>$request->email?$request->email:$sponsor->email,
+                "logo" => $request->file('logo')->store("sponsor_logo","public")
+            ]);
+        }
+        else{
+            $sponsor->update($request->all());
+        }
+
+
+        return $sponsor;
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Sponsor $sponsor)
     {
         //
+        $sponsor->delete();
+        return response("",Response::HTTP_NO_CONTENT);
+
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,10 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\SpeakerController;
+use App\Http\Controllers\SponsorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,8 +20,10 @@ use App\Http\Controllers\SpeakerController;
 Route::middleware("auth:sanctum")->group(function(){
     Route::apiResource("event",EventController::class,["except"=>["index","show"]]);
     Route::apiResource("speaker",SpeakerController::class,["except"=>["show"]]);
+    Route::apiResource("sponsor",SponsorController::class,["except"=>["show"]]);
 });
 Route::apiResource("event",EventController::class,["only"=>["index","show"]]);
 Route::apiResource("speaker",SpeakerController::class,["only"=>["show"]]);
+Route::apiResource("sponsor",SponsorController::class,["only"=>["show"]]);
 Route::post("auth/register",[AuthController::class, "register"]);
 Route::post("auth/login",[AuthController::class, "login"]);
